5c0cb5dcd70d0acd611313ab02fdfe6d06959f86
dxpelou <la.olu@live.co.uk>
2017-06-21 18:38:23 +0100

    google sign in clean up

--- app/src/main/java/com/louanimashaun/fattyzgrill/MealsActivity.java
+++ app/src/main/java/com/louanimashaun/fattyzgrill/MealsActivity.java
@@ -2,55 +2,60 @@ package com.louanimashaun.fattyzgrill;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
-
+import com.louanimashaun.fattyzgrill.data.MealsRepository;
+import com.louanimashaun.fattyzgrill.data.source.local.MealsLocalDataSoure;
+import com.louanimashaun.fattyzgrill.data.source.remote.MealsRemoteDataSource;
+import com.louanimashaun.fattyzgrill.presenter.MealsPresenter;
+import com.louanimashaun.fattyzgrill.view.MealsFragment;
 
 import java.util.Arrays;
 
-public class SignInActivity extends AppCompatActivity {
+public class MealsActivity extends AppCompatActivity {
 
     FirebaseDatabase mFirebaseDatabase;
     FirebaseAuth mFirebaseAuth;
     FirebaseAuth.AuthStateListener mAuthStateListener;
 
-    //Request code
-    public static final int RC_SIGN_IN = 1;
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.meals_act);
 
-        setContentView(R.layout.activity_sign_in);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setUpToolbar();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        MealsFragment mealsFragment = (MealsFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.content_frame);
+
+        if (mealsFragment == null){
+            mealsFragment = MealsFragment.newInstance();
+        }
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.content_frame, mealsFragment);
+        transaction.commit();
+
+        MealsPresenter mMealsPresenter = new MealsPresenter(
+                MealsRepository.getInstance(new MealsLocalDataSoure(), MealsRemoteDataSource.getInstance()), mealsFragment);
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user != null){
-                    Toast.makeText(SignInActivity.this, "You are signed in to fatties mobile app", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MealsActivity.this, "You are signed in to fatties mobile app", Toast.LENGTH_SHORT).show();
                 }else{
                     startActivityForResult(
                             AuthUI.getInstance()
@@ -58,7 +63,7 @@ public class SignInActivity extends AppCompatActivity {
                                     .setIsSmartLockEnabled(false)
                                     .setAvailableProviders(
                                             Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                                                    //,new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                                    //,new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
                                     .build(),
                             RC_SIGN_IN);
                 }
@@ -66,15 +71,8 @@ public class SignInActivity extends AppCompatActivity {
         };
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    private void setUpToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
