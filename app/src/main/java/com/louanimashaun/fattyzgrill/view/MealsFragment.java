770db91e8c6423a2d43dd5e9a888345b9d6ee5c0
dxpelou <la.olu@live.co.uk>
2017-09-03 19:24:36 +0100

    total price functionality

--- app/src/main/java/com/louanimashaun/fattyzgrill/view/MealsFragment.java
+++ app/src/main/java/com/louanimashaun/fattyzgrill/view/MealsFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.louanimashaun.fattyzgrill.contract.BasePresenter;
 import com.louanimashaun.fattyzgrill.contract.MealContract;
